e47f25b70eee9500e2304bb16a0f8a8afd1f6e80
Erik Arens <erikarens@netzhero.de>
2022-01-25 16:04:32 +0100

    Used the recipe constructor to create two hardcoded recipes and added them to show as in the recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://assets.epicurious.com/photos/5c1146159ee3cf630f891235/5:4/w_3330,h_2664,c_limit/spiced-chickpeas-and-greens-frittata-recipe-BA-121218.jpg'
+    ),
+    new Recipe(
+      'Im hungry',
+      'This is simply a test',
+      'https://assets.epicurious.com/photos/5c1146159ee3cf630f891235/5:4/w_3330,h_2664,c_limit/spiced-chickpeas-and-greens-frittata-recipe-BA-121218.jpg'
+    ),
+  ];
 
   constructor() {}
 
