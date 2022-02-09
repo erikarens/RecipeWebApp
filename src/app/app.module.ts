e27ea199d56f35d6057fb1b3920dc33e181d366b
Erik Arens <erikarens@netzhero.de>
2022-02-09 10:04:45 +0100

    Added a Service to handle the Recipes and one to handle the Shopping-List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -23,14 +24,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [ShoppingListService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Mein Test Rezept',
+      'Test Rezepte schmecken gut!',
+      'https://assets.epicurious.com/photos/5c1146159ee3cf630f891235/5:4/w_3330,h_2664,c_limit/spiced-chickpeas-and-greens-frittata-recipe-BA-121218.jpg',
+      [new Ingredient('Fleisch', 1), new Ingredient('Pommes', 20)]
+    ),
+    new Recipe(
+      'Mein Test Rezept Nummer 2',
+      'Test Rezepte schmecken gut!',
+      'https://assets.epicurious.com/photos/5c1146159ee3cf630f891235/5:4/w_3330,h_2664,c_limit/spiced-chickpeas-and-greens-frittata-recipe-BA-121218.jpg',
+      [new Ingredient('Hackbällchen', 1), new Ingredient('Tomaten Sauce', 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
